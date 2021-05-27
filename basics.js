445e7f510af5b795c81cd29626d3889a41dba06a
Brian Abbott <burnedfaceless@gmail.com>
2021-05-27 09:51:08 -0400

    commit

---
+++ basics.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('#canvas')
+const ctx = canvas.getContext('2d')
+
+// ctx.fillStyle = 'blue'
+// ctx.fillRect(100, 100, 300, 300)
+
+ctx.strokeRect(0, 100, 200, 300)
+ctx.clearRect(100, 100, 500, 500)
